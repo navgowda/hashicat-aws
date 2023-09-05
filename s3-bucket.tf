03b61b9fa815197b8fa63bfd43dc90569fe904cf
navgowda <npgowda@amazon.de>
2023-09-05 20:27:55 +0000

    Added s3 bucket module 1

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,13 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "npgowda-s3-bucket"
-  acl    = "private"
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  versioning = {
-    enabled = true
-  }
+module "s3-bucket" {
+  source  = "app.terraform.io/npgowda-org-aws/s3-bucket/aws"
+  version = "2.8.0"
+  bucket_prefix = "npgowda"
 }
